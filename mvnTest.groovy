19374652ddea7d34fc1e23a1e40c7f0f07221576
muddassir <muhammadmuddassir19@gmail.com>
2023-12-03 13:07:05 +0530

    maven tool added

--- mvnTest.groovy
+++ mvnTest.groovy
@@ -1,4 +1,4 @@
 def call(){
     //sh 'mvn test'
-    sh 'mvn -pl "!com.mycom.hp.comp:zonegtools,!com.mycom.hpe:testbed" -P compileWithGradle,nightly,flex-debug clear'
+    sh "mvn  -pl \\!com.mycom.hp.comp:zonegtools"
 }
